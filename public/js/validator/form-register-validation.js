fdc9937ec064b4296bdd26010637f4273840e938
valensartorelli <valen.sartorelli@gmail.com>
2021-07-21 19:33:12 -0300

    Valen - Agrego validaciones de registro

---
+++ public/js/validator/form-register-validation.js
@@ -0,0 +1,195 @@
+window.addEventListener("load", function(){
+    
+    // Capturo elementos
+    let form = document.querySelector("#form-registro");
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    let userName = document.getElementById("userName");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let repassword = document.getElementById("repassword");
+    let avatar = document.getElementById("avatar");
+    let rol = document.querySelector("#rol").selectedIndex;
+    let btnReg = document.getElementById("btn-reg");
+    let errorElement = document.getElementById('error');
+
+    const acceptedExtensions = ['jpg', 'png', 'gif', 'jpeg', 'JPG', 'PNG', 'GIF', 'JPEG'];
+
+    // Eventos
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        //alert("entre aca!")
+
+        let hasErrors = {
+            firstName: nameValidator(),
+            lastName: lastNameValidator(),
+            userName: userNameValidator(),
+            email: emailValidator(),
+            password: passwordValidator(),
+            repassword: repasswordValidator(),
+            avatar: avatarValidator(),
+            rol: rolValidator()
+        }
+
+
+        // Si hay errores, prevengo la accion por defecto
+        if ( hasErrors.firstName || hasErrors.lastName || hasErrors.userName || hasErrors.email || hasErrors.password || hasErrors.repassword || hasErrors.avatar || hasErrors.rol
+            ) e.preventDefault();
+    });
+
+    // Llamo a las funciones
+    firstName.addEventListener('blur', nameValidator);
+    lastName.addEventListener('blur', lastNameValidator);
+    userName.addEventListener('blur', userNameValidator);
+    email.addEventListener('blur', emailValidator);
+    password.addEventListener('blur', passwordValidator);
+    repassword.addEventListener('blur', repasswordValidator);
+    avatar.addEventListener('change', avatarValidator);
+
+    // Escribe el mensaje
+    writeMsg = ( ...arrToWrite ) => {
+        arrToWrite.forEach( elemToWrite => {
+            document.getElementById(elemToWrite.id).innerText = elemToWrite.msg;
+        });
+    }
+
+    // Funciones 
+    function nameValidator () {
+        let id = 'firstName_error';
+        if (firstName.value == '' || firstName.value == null) {
+            writeMsg( { id, msg: 'El nombre no puede estar vacío' } );
+            firstName.classList.add('is-invalid');
+            return true
+        } else if (firstName.value.length < 2) {
+            writeMsg( { id, msg: 'El nombre debe tener al menos 2 caracteres' } );
+            firstName.classList.add('is-invalid');
+            return true
+        }
+        writeMsg( { id, msg: '' } );
+        firstName.classList.remove('is-invalid');
+        return false
+    }
+
+    function lastNameValidator () {
+        let id = 'lastName_error';
+        if (lastName.value == '' || lastName.value == null) {
+            writeMsg( { id, msg: 'El apellido no puede estar vacío' } );
+            lastName.classList.add('is-invalid');
+            return true
+        } else if (lastName.value.length < 2) {
+            writeMsg( { id, msg: 'El apellido debe tener al menos 2 caracteres' } );
+            lastName.classList.add('is-invalid');
+            return true
+        }
+        writeMsg( { id, msg: '' } );
+        lastName.classList.remove('is-invalid');
+        return false
+    }
+
+    function userNameValidator () {
+        let id = 'userName_error';
+        if (userName.value == '' || userName.value == null) {
+            writeMsg( { id, msg: 'El usuario no puede estar vacío' } );
+            userName.classList.add('is-invalid');
+            return true
+        } else if (userName.value.length < 2) {
+            writeMsg( { id, msg: 'El usuario debe tener al menos 2 caracteres' } );
+            userName.classList.add('is-invalid');
+            return true
+        }
+        writeMsg( { id, msg: '' } );
+        userName.classList.remove('is-invalid');
+        return false
+    }
+
+    function emailValidator () {
+        let id = 'email_error';
+        let regexEmail = /^([A-Za-z0-9_\-\+\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+        if (email.value == '' || email.value == null) {
+            writeMsg( { id, msg: 'El email no puede estar vacío' } );
+            email.classList.add('is-invalid');
+            return true
+        } else if (!email.value.match(regexEmail)) {
+            writeMsg( { id, msg: 'El email debe ser valido' } );
+            email.classList.add('is-invalid');
+            return true
+        }
+        writeMsg( { id, msg: '' } );
+        email.classList.remove('is-invalid');
+        return false
+    }
+
+    function passwordValidator () {
+        let id = 'password_error';
+        let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])");
+        if (password.value == '' || password.value == null) {
+            writeMsg( { id, msg: 'La contraseña no puede estar vacía' } );
+            password.classList.add('is-invalid');
+            return true
+        } else if (!password.value.match(strongRegex)) {
+            writeMsg( { id, msg: 'La contraseña debe tener al menos una minúscula, una mayúscula, un número y un caracter especial' } );
+            password.classList.add('is-invalid');
+            return true
+        } else if (password.value.length < 8 || password.value.length > 11) {
+            writeMsg( { id, msg: 'La contraseña debe tener entre 8 y 11 caracteres' } );
+            password.classList.add('is-invalid');
+            return true
+        }
+        writeMsg( { id, msg: '' } );
+        password.classList.remove('is-invalid');
+        return false
+    }
+
+    function repasswordValidator () {
+        let id = 'repassword_error';
+        if (repassword.value == '' || repassword.value == null) {
+            writeMsg( { id, msg: 'Este campo no puede estar vacío' } );
+            repassword.classList.add('is-invalid');
+            return true
+        } else if (repassword.value !== password.value) {
+            writeMsg( { id, msg: 'Las contraseñas no coinciden' } );
+            repassword.classList.add('is-invalid');
+            return true
+        }
+        writeMsg( { id, msg: '' } );
+        repassword.classList.remove('is-invalid');
+        return false
+    }
+
+    function avatarValidator () {
+        const avatarError = document.querySelector("#avatar_error");
+        let feedback = ''; 
+        console.log(avatar);
+        console.log("-----------------------");
+     
+        if (avatar) {
+            let filename = avatar.value;
+            let fileExtension = filename.split(".").pop();
+            if (!acceptedExtensions.includes(fileExtension)) {
+                feedback = `Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}` 
+                avatar.classList.add('is-invalid');   
+            }
+        }
+        if (feedback) {
+            avatarError.innerText = feedback;
+            hasErrors.avatar = feedback;
+        } else{
+            avatarError.innerText = ''; 
+            avatar.classList.remove('is-invalid');  
+        }
+    }
+
+    function rolValidator () {
+        const errorRol = document.querySelector("#rol_error");
+        if( rol == null || rol == 0 ) {	
+            errorRol.innerText = 'Debe seleccionar una categoríaaaa';
+            return true;
+        }
+        else{
+            errorRol.innerText = '';
+            return false;
+        }
+    }
+
+
+})
--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -2,16 +2,28 @@ const path = require('path');
 const { body } = require('express-validator');
 
 module.exports = [
-	body('firstName').notEmpty().withMessage('Tienes que escribir tu nombre'),
-	body('lastName').notEmpty().withMessage('Tienes que escribir tu apellido'),
-	body('userName').notEmpty().withMessage('Tienes que escribir un usuario'),
+	body('firstName').notEmpty().withMessage('Tienes que escribir tu nombre').bail()
+		.isLength({ min: 2 }).withMessage('El nombre debe tener al menos 2 caracteres'),
+	body('lastName').notEmpty().withMessage('Tienes que escribir tu apellido').bail()
+		.isLength({ min: 2 }).withMessage('El apellido debe tener al menos 2 caracteres'),
+	body('userName').notEmpty().withMessage('Tienes que escribir un usuario').bail()
+		.isLength({ min: 2 }).withMessage('El usuario debe tener al menos 2 caracteres'),
 	body('email')
 		.notEmpty().withMessage('Tienes que escribir un correo electrónico').bail()
 		.isEmail().withMessage('Debes escribir un formato de correo válido'),
-	body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+	body('password').notEmpty().withMessage('Tienes que escribir una contraseña').bail()
+		.matches(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA-Z0-9])(?!.*\s).{4,}$/).withMessage('Tu contraseña debe tener al menos una mayúscula, una minúscula, un número y un caracter especial').bail()
+		.isLength({ min: 8, max: 11 }).withMessage('La contraseña debe tener entre 8 y 11 caracteres'),
+	body('repassword').notEmpty().withMessage('Tienes que volver a escribir la contraseña').bail()
+		.custom((value, { req }) => {
+			if(value != req.body.password){
+				throw new Error('Las contraseñas no coinciden');
+			}
+			return true;
+		}),
 	body('avatar').custom((value, { req }) => {
 		let file = req.file;
-		let acceptedExtensions = ['.jpg', '.png', '.gif'];
+		let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.JPG', 'JPEG', '.PNG', '.GIF'];
 
 		if (!file) {
 			throw new Error('Tienes que subir una imagen');
